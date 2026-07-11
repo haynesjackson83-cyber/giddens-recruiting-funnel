e7686bf3de827f734a27b4d43c08a384c2f910e0
haynesjackson83-cyber <haynesjackson83@gmail.com>
2026-07-11 18:11:23 -0500

    Add Giddens Difference homepage section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/hero";
 import { RealityCheck } from "@/components/reality-check";
+import { GiddensDifference } from "@/components/giddens-difference";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <RealityCheck />
+      <GiddensDifference />
     </main>
   );
 }
---
+++ src/components/giddens-difference/index.ts
@@ -0,0 +1,1 @@
+export { GiddensDifference } from "./GiddensDifference";
